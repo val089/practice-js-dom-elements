d0277147a1fab353cab32f4a9a4826b51d151813
val089 <kamilszerlag@gmail.com>
2020-06-29 21:07:19 +0200

    Add a solution

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,14 @@
 console.log('DOM');
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const links = document.querySelectorAll('a');
+    console.log(links);
+
+    links.forEach(link => {
+        if (link.hasAttribute('data-url')) {
+            const url = link.getAttribute('data-url');
+            link.setAttribute('href', url);
+        }
+    });
+});
