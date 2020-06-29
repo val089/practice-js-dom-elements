74f488127f170ffa5b74535135a02e977ac7582c
val089 <kamilszerlag@gmail.com>
2020-06-29 18:48:11 +0200

    Added a solution

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,18 @@
 console.log('DOM');
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const el = document.querySelector('.comments__item.comments__item--newest');
+   
+    const spans = el.querySelectorAll('span');
+    
+    const arr = [...spans];
+    
+    const dataInfo = arr.filter(el => {
+        if(el.hasAttribute('data-info')) {
+            return el;
+        }
+    })
+
+    console.log(dataInfo.length);
+});
