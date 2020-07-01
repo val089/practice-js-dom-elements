f937c410f27c04ad1f126b2be7ce4af6add7bbe7
val089 <kamilszerlag@gmail.com>
2020-07-01 11:21:25 +0200

    Added a solution

--- 03/app.js
+++ 03/app.js
@@ -1,4 +1,8 @@
-console.log('DOM');
+// console.log('DOM');
+
+const parentForButton = document.querySelector('.parent-for-button');
+const btn = document.createElement('button');
+parentForButton.appendChild(btn);
 
 const buttonSettings = {
     attr: {
@@ -11,4 +15,31 @@ const buttonSettings = {
         color: '#444'
     },
     text: 'Click me!',
+}
+
+for (const key in buttonSettings) {
+    if (buttonSettings.hasOwnProperty(key)) {
+        // console.log(key);
+        // console.log(key, buttonSettings[key]);
+
+        if (key === 'attr') {
+            btn.classList.add(buttonSettings.attr.className);
+            btn.title = buttonSettings.attr.title;
+        } 
+        else if (key === 'css') {
+            const cssButtonSettings = buttonSettings[key];
+            console.log(cssButtonSettings);
+
+            for (const cssKey in cssButtonSettings) {
+                btn.style[cssKey] = cssButtonSettings[cssKey];
+            }
+
+        // btn.style.border = cssSettings.css.border
+        // btn.style.padding = cssSettings.css.padding
+        // btn.style.backgroundColor = cssSettings.css.color
+        } 
+        else {
+            btn.textContent = buttonSettings.text; 
+        }
+    }
 }
